b662f2f28b69a7123395ad668a48ab43c0f9afff
Kjames3 <bestofkamren@gmail.com>
2025-10-08 13:41:07 -0700

    Fixed traffic generation

--- generate_traffic.py
+++ generate_traffic.py
@@ -1,7 +1,4 @@
-# ==============================================================================
-# -- find carla module ---------------------------------------------------------
-# ==============================================================================
-
+# Find the Carla module from running instance
 
 import glob
 import os
@@ -16,11 +13,12 @@ except IndexError:
     pass
 
 
-# Imports 
+
+# Import carla and other modules
 import carla
 import random
 
-# Connect to the client and retrieve the world object
+# Connect to the local client and retrieve the world object
 client = carla.Client('localhost', 2000)
 world = client.get_world()
 
@@ -40,3 +38,40 @@ random.seed(0)
 
 # We will aslo set up the spectator so we can see what we do
 spectator = world.get_spectator()
+
+# Get spawn points
+spawn_points = world.get_map().get_spawn_points()
+
+
+# Draw the spawn point locations as numbers in the map
+for i, spawn_point in enumerate(spawn_points):
+    world.debug.draw_string(spawn_point.location, str(i), life_time=10)
+
+
+# Select some models from the blueprint library
+models = ['dodge', 'audi', 'model3', 'mini', 'mustang', 'lincoln', 'prius', 'nissan', 'crown', 'impala']
+blueprints = []
+for vehicle in world.get_blueprint_library().filter('*vehicle*'):
+    if any(model in vehicle.id for model in models):
+        blueprints.append(vehicle)
+
+# Set a max number of vehicles and prepare a list for those we spawn
+max_vehicles = 50
+max_vehicles = min([max_vehicles, len(spawn_points)])
+vehicles = []
+
+# Take a random sample of the spawn points and spawn some vehicles
+for i, spawn_point in enumerate(random.sample(spawn_points, max_vehicles)):
+    temp = world.try_spawn_actor(random.choice(blueprints), spawn_point)
+    if temp is not None:
+        vehicles.append(temp)
+
+for vehicle in vehicles:
+    vehicle.set_autopilot(True)
+    # Randomly set the probability that a vehicle will ignore traffic lights
+    traffic_manager.ignore_lights_percentage(vehicle, random.randint(0,50))
+
+
+# Run the simulation so we can inspect the results with the spectator
+while True:
+    world.tick()
